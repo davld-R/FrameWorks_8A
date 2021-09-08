da39e6b90c6d05ab32b6bb137db785ccf9331297
davld-R <juanrojas043@gmail.com>
2021-09-08 08:06:43 -0500

    carga inicial de los scripts python suma y math_oper

---
+++ scrips_python/math_oper.py
@@ -0,0 +1,10 @@
+a = 5
+b = 10
+resta = a - b
+suma = a + b
+mult = a * b
+div = a / b
+print("La resta es: ", resta)
+print("The addition is: ", suma)
+print("The mult is: ", mult)
+print("The div is: ", div)
