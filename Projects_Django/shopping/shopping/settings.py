5007d7a23e64b0c934f1dede99f1e33205469bdb
davld-R <juanrojas043@gmail.com>
2021-09-28 09:11:00 -0500

    28 de sept del 2021

--- Projects_Django/shopping/shopping/settings.py
+++ Projects_Django/shopping/shopping/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shopping',
         'USER': 'postgres',
-        'PASSWORD': 'DsJnTv20DS',
+        'PASSWORD': 'Password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     },
