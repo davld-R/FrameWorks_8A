6fcec50249aabb304063847c0da5a1c0d148f90e
davld-R <juanrojas043@gmail.com>
2021-10-09 01:12:17 -0500

    Taller Práctico Corte 2

---
+++ Projects_Django/MyPets/tienda_mascotas/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+# from .models import race
+# admin.site.register(race)
+
+# from .models import country
+# admin.site.register(country)
+
+# from .models import city
+# admin.site.register(city)
--- Projects_Django/shopping/clothes/models.py
+++ Projects_Django/shopping/clothes/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 class Marca(models.Model) :
     nombre = models.CharField(max_length=100)
     pais = models.CharField(max_length=100)
-    tipo = models.CharField(max_length=100)
+    tipo_marca = models.CharField(max_length=100)
     estado = models.IntegerField(default=1)
+    #Aqui van las llaves foraneas
 
 class Sucursal(models.Model) :
     ciudad = models.CharField(max_length=100)
-  
+    direccion = models.CharField(max_length=100, default= "")
+
+class Cliente(models.Model) :
+    nombre = models.CharField(max_length=100)
